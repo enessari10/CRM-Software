8824a12461e3eb188ac0e8ba6890745fb4638f5a
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-18 22:59:55 +0300

    Home fix

--- pages/admin/home.php
+++ pages/admin/home.php
@@ -1,4 +1,45 @@
-<?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_success_modal.html") ?>
+<?php 
+
+  function popup() {
+    echo '<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <title>Bootstrap Example</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    
+    <div class="container">
+     
+      <!-- Modal -->
+      <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+        
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Modal Header</h4>
+            </div>
+            <div class="modal-body">
+              <p>Some text in the modal.</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      
+    </div>';
+  }
+
+?>
 
 <!DOCTYPE html>
 <html lang="tr">
@@ -91,7 +132,7 @@
                 <div class="card">
                   <div class="card-body">
                     <h4 class="card-title">Takvim </h4>
-                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" id="myModal1">Open Modal</button>
+                    <a href="<?php popup(); ?>">TIKLA </a>
                     <!--Takvim -->
                   </div>
                 </div>
