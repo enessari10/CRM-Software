8f8b7cbe2bf070db6c014985486c762b24f1777d
yusufsinan0 <ysufsinan@gmail.com>
2022-10-18 16:17:06 +0300

    customer menu fix

--- pages/customer/home.php
+++ pages/customer/home.php
@@ -7,8 +7,8 @@
       <!-- partial:partials/_navbar.html -->
       <nav class="navbar d  efault-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-        <a class="navbar-brand brand-logo" href="index.html"><img src="/assets/images/logo.png" alt="logo" /></a>
-          <a class="navbar-brand brand-logo-mini" href="index.html"><img src="/assets/images/logo-mini.svg" alt="logo" /></a>
+        <a class="navbar-brand brand-logo" href="home.php"><img src="/assets/images/logo.png" alt="logo" /></a>
+          <a class="navbar-brand brand-logo-mini" href="home.php"><img src="/assets/images/logo-mini.svg" alt="logo" /></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-stretch">
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
