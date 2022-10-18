48feda1c3dafcdd9f976db5ef20e5911ce65bf05
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-18 12:49:01 +0300

    login convert tr

--- pages/login.php
+++ pages/login.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="tr">
   <head>
   <?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_header.html") ?>
   </head>
@@ -13,31 +13,30 @@
                 <div class="brand-logo">
                   <img src="../../assets/images/logo.svg">
                 </div>
-                <h4>Hello! let's get started</h4>
-                <h6 class="font-weight-light">Sign in to continue.</h6>
+                <h4>Merhaba, CRM Sistemine hoş geldiniz</h4>
+                <h6 class="font-weight-light">Devam etmek için oturum açın.</h6>
                 <form class="pt-3">
                   <div class="form-group">
-                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+                    <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Email Adresi">
                   </div>
                   <div class="form-group">
-                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                    <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Şifre">
                   </div>
                   <div class="mt-3">
-                    <a class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">SIGN IN</a>
+                    <a class="btn btn-block btn-gradient-primary btn-lg font-weight-medium auth-form-btn" href="../../index.html">GİRİŞ YAP</a>
                   </div>
                   <div class="my-2 d-flex justify-content-between align-items-center">
                     <div class="form-check">
                       <label class="form-check-label text-muted">
-                        <input type="checkbox" class="form-check-input"> Keep me signed in </label>
+                        <input type="checkbox" class="form-check-input"> Oturumumu açık tut </label>
                     </div>
-                    <a href="#" class="auth-link text-black">Forgot password?</a>
+                    <a href="#" class="auth-link text-black">Şifremi Unuttum?</a>
                   </div>
                   <div class="mb-2">
-                    <button type="button" class="btn btn-block btn-facebook auth-form-btn">
-                      <i class="mdi mdi-facebook me-2"></i>Connect using facebook </button>
+                   
                   </div>
-                  <div class="text-center mt-4 font-weight-light"> Don't have an account? <a href="register.html" class="text-primary">Create</a>
-                  </div>
+                  <!-- <div class="text-center mt-4 font-weight-light"> Don't have an account? <a href="register.html" class="text-primary">Create</a>
+--> </div>
                 </form>
               </div>
             </div>
