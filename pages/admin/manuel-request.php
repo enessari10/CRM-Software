a1562042cba8e9bcd7903044f7852c881b3b6b53
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-18 16:34:52 +0300

    Manuel request fix

--- pages/admin/manuel-request.php
+++ pages/admin/manuel-request.php
@@ -186,56 +186,26 @@
                 </ul>
               </nav>
             </div>
-            
-           
-             
             <div class="row">
-              
             <div class="col-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <h4 class="card-title">Basic form elements</h4>
-                    <p class="card-description"> Basic form elements </p>
+                    <h4 class="card-title">Manuel Talep Oluşturma</h4>
                     <form class="forms-sample">
                       <div class="form-group">
-                        <label for="exampleInputName1">Name</label>
-                        <input type="text" class="form-control" id="exampleInputName1" placeholder="Name">
+                        <label for="exampleInputName1">Başlık</label>
+                        <input type="text" class="form-control" id="baslik" placeholder="Başlık">
                       </div>
                       <div class="form-group">
-                        <label for="exampleInputEmail3">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail3" placeholder="Email">
+                        <label for="exampleInputEmail3">Açıklama</label>
+                        <input type="email" class="form-control" id="aciklama" placeholder="Açıklama">
                       </div>
                       <div class="form-group">
-                        <label for="exampleInputPassword4">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword4" placeholder="Password">
+                        <label for="exampleInputPassword4">Tarih / Zaman</label>
+                        <input type="datetime-local" class="form-control" id="tarih" placeholder="Tarih">
                       </div>
-                      <div class="form-group">
-                        <label for="exampleSelectGender">Gender</label>
-                        <select class="form-control" id="exampleSelectGender">
-                          <option>Male</option>
-                          <option>Female</option>
-                        </select>
-                      </div>
-                      <div class="form-group">
-                        <label>File upload</label>
-                        <input type="file" name="img[]" class="file-upload-default">
-                        <div class="input-group col-xs-12">
-                          <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
-                          <span class="input-group-append">
-                            <button class="file-upload-browse btn btn-gradient-primary" type="button">Upload</button>
-                          </span>
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputCity1">City</label>
-                        <input type="text" class="form-control" id="exampleInputCity1" placeholder="Location">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleTextarea1">Textarea</label>
-                        <textarea class="form-control" id="exampleTextarea1" rows="4"></textarea>
-                      </div>
-                      <button type="submit" class="btn btn-gradient-primary me-2">Submit</button>
-                      <button class="btn btn-light">Cancel</button>
+                      <button type="submit" class="btn btn-gradient-primary me-2">Talep Oluştur</button>
+                      <button class="btn btn-light">Anasayfa</button>
                     </form>
                   </div>
                 </div>
