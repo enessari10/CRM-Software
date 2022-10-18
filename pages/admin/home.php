6ae8b62e257579279c51f3ea096e9535cb64f418
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-18 23:20:27 +0300

    Fix

--- pages/admin/home.php
+++ pages/admin/home.php
@@ -1,3 +1,5 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_popup.html") ?>
+
 <!DOCTYPE html>
 <html lang="tr">
   <head>
@@ -90,29 +92,8 @@
                   <div class="card-body">
                     <h4 class="card-title">Takvim </h4>
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-                    <div class="modal" id="myModal">
-    <div class="modal-dialog">
-      <div class="modal-content">
-      
-        <!-- Modal Header -->
-        <div class="modal-header">
-          <h4 class="modal-title">Modal Heading</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-        
-        <!-- Modal body -->
-        <div class="modal-body">
-          Modal body..
-        </div>
-        
-        <!-- Modal footer -->
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-        </div>
-        
-      </div>
-    </div>
-  </div>
+    Open modal
+  </button>
                     <!--Takvim -->
                   </div>
                 </div>
