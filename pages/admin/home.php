38c2f204b576f2f68c93f158832a8572643ba850
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-19 00:37:29 +0300

    Test button

--- pages/admin/home.php
+++ pages/admin/home.php
@@ -1,3 +1,11 @@
+<?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_popup.php") ?>
+
+<?php 
+if(isset($_POST['button1'])) {
+  successPopup();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="tr">
   <head>
@@ -92,24 +100,13 @@
                     <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
   Launch demo modal
 </button><div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+<form method="post">
+        <input type="submit" name="button1"
+                value="Button1"/>
+          
+        <input type="submit" name="button2"
+                value="Button2"/>
+    </form>
                     <!--Takvim -->
                   </div>
                 </div>
