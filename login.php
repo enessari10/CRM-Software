64a42216eb6ae7cfd8482dbc0680c4d2c22068c2
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-18 15:37:17 +0300

    Fix logos

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
             <div class="col-lg-4 mx-auto">
               <div class="auth-form-light text-left p-5">
                 <div class="brand-logo">
-                  <img src="../../assets/images/logo.svg">
+                  <img src="/assets/images/logo.png">
                 </div>
                 <h4>Merhaba, CRM Sistemine hoş geldiniz</h4>
                 <h6 class="font-weight-light">Devam etmek için oturum açın.</h6>
