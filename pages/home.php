9b7d2300ca03d27f003a6f4dc754888d05ecfd80
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-18 14:53:28 +0300

    Footer added home

--- pages/home.php
+++ pages/home.php
@@ -293,12 +293,8 @@
              
           <!-- content-wrapper ends -->
           <!-- partial:partials/_footer.html -->
-          <footer class="footer">
-            <div class="container-fluid d-flex justify-content-between">
-              <span class="text-muted d-block text-center text-sm-start d-sm-inline-block"> </span>
-              <span class="float-none float-sm-end mt-1 mt-sm-0 text-end"> Copyright <a href="https://www.mikroes.com/" target="_blank"> Mikroes  Bilişim ve Danışmanlık Hizmetleri </a>2022</span>
-            </div>
-          </footer>
+          <?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_footer.html") ?>
+
           <!-- partial -->
         </div>
         <!-- main-panel ends -->
