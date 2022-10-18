ef809f95f5f1c5f946fa9f452a896edf3254704b
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-18 15:15:31 +0300

    Home menu and user fix

--- pages/home.php
+++ pages/home.php
@@ -28,7 +28,7 @@
                   <span class="availability-status online"></span>
                 </div>
                 <div class="nav-profile-text">
-                  <p class="mb-1 text-black">David Greymaax</p>
+                  <p class="mb-1 text-black">İsim Soyisim</p>
                 </div>
               </a>
               <div class="dropdown-menu navbar-dropdown" aria-labelledby="profileDropdown">
@@ -161,8 +161,8 @@
                   <!--change to offline or busy as needed-->
                 </div>
                 <div class="nav-profile-text d-flex flex-column">
-                  <span class="font-weight-bold mb-2">David Grey. H</span>
-                  <span class="text-secondary text-small">Project Manager</span>
+                  <span class="font-weight-bold mb-2">Email Adresi</span>
+                  <span class="text-secondary text-small">Firma Adı</span>
                 </div>
                 <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
               </a>
