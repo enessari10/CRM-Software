b55efcc4c0af2afa7db54294f7ca2d77e74304e9
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-18 23:10:54 +0300

    Home modal new and header added

--- pages/admin/home.php
+++ pages/admin/home.php
@@ -1,46 +1,3 @@
-<?php 
-
-  function popup() {
-    echo '<!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <title>Bootstrap Example</title>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    </head>
-    <body>
-    
-    <div class="container">
-     
-      <!-- Modal -->
-      <div class="modal fade" id="myModal" role="dialog">
-        <div class="modal-dialog">
-        
-          <!-- Modal content-->
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-              <h4 class="modal-title">Modal Header</h4>
-            </div>
-            <div class="modal-body">
-              <p>Some text in the modal.</p>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-      
-    </div>';
-  }
-
-?>
-
 <!DOCTYPE html>
 <html lang="tr">
   <head>
@@ -132,7 +89,7 @@
                 <div class="card">
                   <div class="card-body">
                     <h4 class="card-title">Takvim </h4>
-                    <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="<?php popup(); ?>">Open Modal</button>
+                    <p><a href="#ex1" rel="modal:open">Open Modal</a></p>
 
                     <!--Takvim -->
                   </div>
@@ -152,6 +109,10 @@
       </div>
       <!-- page-body-wrapper ends -->
     </div>
+    <div id="ex1" class="modal">
+  <p>Thanks for clicking. That felt good.</p>
+  <a href="#" rel="modal:close">Close</a>
+</div>
     <!-- container-scroller -->
     <!-- plugins:js -->
     <script src="/assets/vendors/js/vendor.bundle.base.js"></script>
@@ -172,4 +133,5 @@
 
     <!-- End custom js for this page -->
   </body>
+  
 </html>
