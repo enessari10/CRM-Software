544d69b253b84a8ae4554293c58590573a4eec60
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-19 23:13:28 +0300

    Chat fix

--- pages/admin/chat-room.php
+++ pages/admin/chat-room.php
@@ -1,28 +1,11 @@
 <html>
 <head>
 	<title>Chat application in php using web scocket programming</title>
-	<!-- Bootstrap core CSS -->
-    <link href="vendor-front/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_header.html") ?>
 
-    <link href="vendor-front/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-    <link rel="stylesheet" type="text/css" href="vendor-front/parsley/parsley.css"/>
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor-front/jquery/jquery.min.js"></script>
-    <script src="vendor-front/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor-front/jquery-easing/jquery.easing.min.js"></script>
-
-    <script type="text/javascript" src="vendor-front/parsley/dist/parsley.min.js"></script>
 	<style type="text/css">
 		html,
-		body {
-		  height: 100%;
-		  width: 100%;
-		  margin: 0;
-		}
+		
 		#wrapper
 		{
 			display: flex;
@@ -100,7 +83,7 @@
 			</div>
 			<div class="col-lg-4">
 			
-				<input type="hidden" name="login_user_id" id="login_user_id" value="<?php echo $login_user_id; ?>" />
+				<input type="hidden" name="login_user_id" id="login_user_id" value="1" />
 				<div class="mt-3 mb-3 text-center">
 					<img src="" width="150" class="img-fluid rounded-circle img-thumbnail" />
 					<h3 class="mt-2">ENES NAME</h3>
