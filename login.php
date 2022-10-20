833cd0c8b549cece53d3de6201608b0e5a6f092b
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-20 11:22:20 +0300

    Login session 2

--- login.php
+++ login.php
@@ -2,57 +2,49 @@
 <?php
 include($_SERVER["DOCUMENT_ROOT"] . "/config/Database.php");
 
+if( isset($_SESSION['email']) ){
 
-// Check if $_SESSION or $_COOKIE already set
-if( isset($_SESSION['userid']) ){
    header('Location: /pages/admin/home.php');
    exit;
-}else if( isset($_COOKIE['rememberme']  )){
+
+} else if( isset($_COOKIE['rememberme']  )){
     
-    // Decrypt cookie variable value
     $userid = decryptCookie($_COOKIE['rememberme']);
-    
     $sql_query = "select * from Users where user_id='".$userid."'";
     $result = mysqli_query($db,$sql_query);
     $row = mysqli_fetch_array($result);
-    $count=mysqli_num_rows($result);
+    $count= mysqli_num_rows($result);
 
     if( $count > 0 ){
+
         $_SESSION['userid'] = $userid;
         $_SESSION['email'] = $row['email'];
-
        header('Location: /pages/admin/home.php');
        exit;
+
     }
 }
 
 
-// Encrypt cookie
 function encryptCookie( $value ) {
 
    $key = hex2bin(openssl_random_pseudo_bytes(4));
-
    $cipher = "aes-256-cbc";
    $ivlen = openssl_cipher_iv_length($cipher);
    $iv = openssl_random_pseudo_bytes($ivlen);
-
    $ciphertext = openssl_encrypt($value, $cipher, $key, 0, $iv);
-
    return( base64_encode($ciphertext . '::' . $iv. '::' .$key) );
 }
 
-// Decrypt cookie
 function decryptCookie( $ciphertext ) {
 
    $cipher = "aes-256-cbc";
-
    list($encrypted_data, $iv,$key) = explode('::', base64_decode($ciphertext));
    return openssl_decrypt($encrypted_data, $cipher, $key, 0, $iv);
 
 }
 
 
-// On submit
 if(isset($_POST['but_submit'])){
 
     $uname = mysqli_real_escape_string($db,$_POST['email']);
@@ -66,12 +58,12 @@ if(isset($_POST['but_submit'])){
 		    $count=mysqli_num_rows($result);
 		
         if($count > 0){
+
              $userid = $row['user_id'];
              $userEmail = $row['email'];
 
             if( isset($_POST['rememberme']) ){
 
-                // Set cookie variables
                 $days = 30;
                 $value = encryptCookie($userid);
                 setcookie ("rememberme",$value,time()+ ($days *  24 * 60 * 60 * 1000));
