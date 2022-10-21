528308a5757dcf4d0182b06af13627df2b9ea351
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-21 12:10:45 +0300

    Class fix

--- ajax/class.php
+++ ajax/class.php
@@ -20,8 +20,9 @@ class Process {
 
         $result =  mysqli_query($db,"SELECT COUNT(*) FROM Firmalar where DATEDIFF(Firmalar.antivirus_tarihi ,NOW()) <= 15;");
         while($row = mysqli_fetch_assoc($result)) {
-            return $row['COUNT(*)'];
+            $count = $row['COUNT(*)'];
         }
+        return $count;
 
     }
 
@@ -29,8 +30,9 @@ class Process {
 
         $result =  mysqli_query($db,"SELECT COUNT(*) FROM Firmalar where DATEDIFF(Firmalar.berqnet_tarihi ,NOW()) <= 15;");
         while($row = mysqli_fetch_assoc($result)) {
-            return $row['COUNT(*)'];
+            $count = $row['COUNT(*)'];
         }
+        return $count;
 
     }
 
@@ -38,17 +40,19 @@ class Process {
 
         $result =  mysqli_query($db,"SELECT COUNT(*) FROM Firmalar where DATEDIFF(Firmalar.web_tarihi ,NOW()) <= 15;");
         while($row = mysqli_fetch_assoc($result)) {
-            return $row['COUNT(*)'];
+            $count = $row['COUNT(*)'];
         }
+        return $count;
 
     }
 
     public function getWaitingServiceCount($db) {
        
-        $talepSQL =  mysqli_query($db,"SELECT COUNT(*) FROM Talepler where talep_durum='Bekliyor'");
-        while($row = mysqli_fetch_assoc($talepSQL)) {
-           return $row['COUNT(*)'];
+        $result =  mysqli_query($db,"SELECT COUNT(*) FROM Talepler where talep_durum='Bekliyor'");
+        while($row = mysqli_fetch_assoc($result)) {
+           $count = $row['COUNT(*)'];
         }
+        return $count;
     }
 
     public function successAlert($message) {
