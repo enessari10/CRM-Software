14cebbd8ea55e2a59bc96f39e48e9eb294d9d345
yusufsinan0 <ysufsinan@gmail.com>
2022-10-29 16:39:16 +0300

    worker all service request php fixed

--- pages/worker/all-service-requests.php
+++ pages/worker/all-service-requests.php
@@ -1,8 +1,18 @@
+<?php 
+  require_once($_SERVER["DOCUMENT_ROOT"].'/config/Database.php');
+  require_once($_SERVER["DOCUMENT_ROOT"].'/ajax/class.php');
+  $processClass = new Process();
+?>
+
 <!DOCTYPE html>
 <html lang="tr">
   <head>
-    <?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_header.php") ?>
-  </head>
+  <?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_header.php") ?>
+     
+     <link rel="stylesheet" href="/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+     <link rel="stylesheet" href="/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+     <link rel="stylesheet" href="/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+       </head>
   <body>
       <!-- partial:partials/_navbar.html -->
       <?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_navbar.php") ?>
@@ -48,7 +58,8 @@
             <div class="col-lg-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-            <table class="table table-bordered">
+          
+                  <table id="example1" class="table table-bordered table-striped">
                       <thead>
                         <tr>
                           <th> Talep Eden </th>
@@ -59,93 +70,18 @@
                         </tr>
                       </thead>
                       <tbody>
-                        <tr>
-                          <td> Test Kullanıcısı </td>
-                          <td> Deneme </td>
-                          <td> 10.10.2022 </td>
-                          <td> <label class="badge badge-danger">Pending</label> </td>
-                          <td>       <div class="btn-group">
-                              <button type="button" class="btn btn-gradient-primary btn-sm" data-bs-toggle="dropdown">İşlem Seç</button>
-                              <div class="dropdown-menu">
-                                <a class="dropdown-item">Durum değiştir</a>
-                                <a class="dropdown-item">Sil</a>
-                              </div>
-                            </div></td>
-                        </tr>
-                        <tr>
-                          <td> Test Kullanıcısı </td>
-                          <td> Deneme </td>
-                          <td> 10.10.2022 </td>
-                          <td> <label class="badge badge-danger">Pending</label> </td>
-                          <td>       <div class="btn-group">
-                              <button type="button" class="btn btn-gradient-primary btn-sm" data-bs-toggle="dropdown">İşlem Seç</button>
-                              <div class="dropdown-menu">
-                                <a class="dropdown-item">Durum değiştir</a>
-                                <a class="dropdown-item">Sil</a>
-                              </div>
-                            </div></td>
-                        </tr>
-                        <tr>
-                          <td> Test Kullanıcısı </td>
-                          <td> Deneme </td>
-                          <td> 10.10.2022 </td>
-                          <td> <label class="badge badge-danger">Pending</label> </td>
-                          <td>       <div class="btn-group">
-                              <button type="button" class="btn btn-gradient-primary btn-sm" data-bs-toggle="dropdown">İşlem Seç</button>
-                              <div class="dropdown-menu">
-                                <a class="dropdown-item">Durum değiştir</a>
-                                <a class="dropdown-item">Sil</a>
-                              </div>
-                            </div></td>
-                        </tr>
-                        <tr>
-                          <td> Test Kullanıcısı </td>
-                          <td> Deneme </td>
-                          <td> 10.10.2022 </td>
-                          <td> <label class="badge badge-danger">Pending</label> </td>
-                          <td>       <div class="btn-group">
-                              <button type="button" class="btn btn-gradient-primary btn-sm" data-bs-toggle="dropdown">İşlem Seç</button>
-                              <div class="dropdown-menu">
-                                <a class="dropdown-item">Durum değiştir</a>
-                                <a class="dropdown-item">Sil</a>
-                              </div>
-                            </div></td>
-                        </tr>
-                        <tr>
-                          <td> Test Kullanıcısı </td>
-                          <td> Deneme </td>
-                          <td> 10.10.2022 </td>
-                          <td> <label class="badge badge-danger">Pending</label> </td>
-                          <td>       <div class="btn-group">
-                              <button type="button" class="btn btn-gradient-primary btn-sm" data-bs-toggle="dropdown">İşlem Seç</button>
-                              <div class="dropdown-menu">
-                                <a class="dropdown-item">Durum değiştir</a>
-                                <a class="dropdown-item">Sil</a>
-                              </div>
-                            </div></td>
-                        </tr>
-                        <tr>
-                          <td> Test Kullanıcısı </td>
-                          <td> Deneme </td>
-                          <td> 10.10.2022 </td>
-                          <td> <label class="badge badge-danger">Pending</label> </td>
-                          <td> 
-                            <div class="btn-group">
-                              <button type="button" class="btn btn-gradient-primary btn-sm" data-bs-toggle="dropdown">İşlem Seç</button>
-                              <div class="dropdown-menu">
-                                <a class="dropdown-item">Durum değiştir</a>
-                                <a class="dropdown-item">Sil</a>
-                              </div>
-                            </div>
-                             </td>
-                        </tr>
+                      <?php 
+              echo $processClass->getAllServiceRequests($db);
+            ?>
                       </tbody>
                     </table>
                     </div>
                 </div>
             </div>
              
-          <!-- content-wrapper ends -->
+                 
+             
+           <!-- content-wrapper ends -->
           <!-- partial:partials/_footer.html -->
           <?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_footer.html") ?>
 
@@ -155,22 +91,7 @@
       </div>
       <!-- page-body-wrapper ends -->
     </div>
-    <!-- container-scroller -->
-    <!-- plugins:js -->
-    <script src="/assets/vendors/js/vendor.bundle.base.js"></script>
-    <!-- endinject -->
-    <!-- Plugin js for this page -->
-    <script src="/assets/vendors/chart.js/Chart.min.js"></script>
-    <script src="/assets/js/jquery.cookie.js" type="text/javascript"></script>
-    <!-- End plugin js for this page -->
-    <!-- inject:js -->
-    <script src="/assets/js/off-canvas.js"></script>
-    <script src="/assets/js/hoverable-collapse.js"></script>
-    <script src="/assets/js/misc.js"></script>
-    <!-- endinject -->
-    <!-- Custom js for this page -->
-    <script src="/assets/js/dashboard.js"></script>
-    <script src="/assets/js/todolist.js"></script>
-    <!-- End custom js for this page -->
+    <?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_footer_script.html") ?>
+
   </body>
 </html>
