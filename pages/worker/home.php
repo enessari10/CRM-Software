e0c031f9f25490d95f3ec648ed20d70503f7b08e
yusufsinan0 <ysufsinan@gmail.com>
2022-10-31 20:37:11 +0300

    worker home session fix

--- pages/worker/home.php
+++ pages/worker/home.php
@@ -3,6 +3,9 @@
 require_once($_SERVER["DOCUMENT_ROOT"].'/config/Database.php');
 require_once($_SERVER["DOCUMENT_ROOT"].'/ajax/class.php');
 $processClass = new Process();
+
+session_start();
+
 if($_SESSION['role'] != 'mikroes_worker') {
   header("location:/../error-404.html");
 }
