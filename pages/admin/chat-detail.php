dd7d3f49cfca2d120379938dbcffe683fb243c9c
Enes Sarı <35381133+enessari10@users.noreply.github.com>
2022-10-31 21:43:31 +0300

    Chat details added

--- pages/admin/chat-detail.php
+++ pages/admin/chat-detail.php
@@ -12,46 +12,131 @@
   <head>
     <?php include($_SERVER["DOCUMENT_ROOT"] . "/partials/_header.php") ?>
     <style>
-
-.main {
-	height: 100vh;
-	padding-top: 70px;
+.chat-box {
+    height: 100%;
+    width: 100%;
+    background-color: #fff;
+    overflow: hidden
 }
 
-.chat-log {
-	padding: 40px 0 114px;
-	height: auto;
-	overflow: auto;
-}
-.chat-log__item {
-	background: #fafafa;
-	padding: 10px;
-	margin: 0 auto 20px;
-	max-width: 80%;
-	float: left;
-	border-radius: 4px;
-	box-shadow: 0 1px 2px rgba(0,0,0,.1);
-	clear: both;
+.chats {
+    padding: 30px 15px
 }
 
-.chat-log__item.chat-log__item--own {
-	float: right;
-	background: #DCF8C6;
-	text-align: right;
+.chat-avatar {
+    float: right
 }
 
-.chat-form {
-	background: #DDDDDD;
-	padding: 40px 0;
-	position: fixed;
-	bottom: 0;
-	width: 100%;
+.chat-avatar .avatar {
+    width: 30px
+        -webkit-box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2),0 6px 10px 0 rgba(0,0,0,0.3);
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,0.2),0 6px 10px 0 rgba(0,0,0,0.3);
 }
 
-.chat-log__author {
-	margin: 0 auto .5em;
-	font-size: 14px;
-	font-weight: bold;
+.chat-body {
+    display: block;
+    margin: 10px 30px 0 0;
+    overflow: hidden
+}
+
+.chat-body:first-child {
+    margin-top: 0
+}
+
+.chat-content {
+    position: relative;
+    display: block;
+    float: right;
+    padding: 8px 15px;
+    margin: 0 20px 10px 0;
+    clear: both;
+    color: #fff;
+    background-color: #62a8ea;
+    border-radius: 4px;
+        -webkit-box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37);
+    box-shadow: 0 1px 4px 0 rgba(0,0,0,0.37);
+}
+
+.chat-content:before {
+    position: absolute;
+    top: 10px;
+    right: -10px;
+    width: 0;
+    height: 0;
+    content: '';
+    border: 5px solid transparent;
+    border-left-color: #62a8ea
+}
+
+.chat-content>p:last-child {
+    margin-bottom: 0
+}
+
+.chat-content+.chat-content:before {
+    border-color: transparent
+}
+
+.chat-time {
+    display: block;
+    margin-top: 8px;
+    color: rgba(255, 255, 255, .6)
+}
+
+.chat-left .chat-avatar {
+    float: left
+}
+
+.chat-left .chat-body {
+    margin-right: 0;
+    margin-left: 30px
+}
+
+.chat-left .chat-content {
+    float: left;
+    margin: 0 0 10px 20px;
+    color: #76838f;
+    background-color: #dfe9ef
+}
+
+.chat-left .chat-content:before {
+    right: auto;
+    left: -10px;
+    border-right-color: #dfe9ef;
+    border-left-color: transparent
+}
+
+.chat-left .chat-content+.chat-content:before {
+    border-color: transparent
+}
+
+.chat-left .chat-time {
+    color: #a3afb7
+}
+
+.panel-footer {
+    padding: 0 30px 15px;
+    background-color: transparent;
+    border-top: 1px solid transparent;
+    border-bottom-right-radius: 3px;
+    border-bottom-left-radius: 3px;
+}
+.avatar img {
+    width: 100%;
+    max-width: 100%;
+    height: auto;
+    border: 0 none;
+    border-radius: 1000px;
+}
+.chat-avatar .avatar {
+    width: 30px;
+}
+.avatar {
+    position: relative;
+    display: inline-block;
+    width: 40px;
+    white-space: nowrap;
+    border-radius: 1000px;
+    vertical-align: bottom;
 }
     </style>
   <body>
@@ -99,36 +184,81 @@
             <div class="col-lg-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-
-                  <header class="page-header">
-  <div class="container ">
-    <h2>Felipe</h2>
-  </div>
-</header>
-<div class="main">
-  <div class="container ">
-    <div class="chat-log">
-        <?php 
-        $id = $_GET['chat_id'];
-        echo  $processClass->getChatDetail($db, $id); ?>
-      <!-- DATA BURAYA-->
+                  <div class="container bootstrap snippets bootdeys">
+<div class="col-md-7 col-xs-12 col-md-offset-2">
+  <!-- Panel Chat -->
+  <div class="panel" id="chat">
+    <div class="panel-heading">
+      <h3 class="panel-title">
+        <i class="icon wb-chat-text" aria-hidden="true"></i> Chat
+      </h3>
     </div>
-  </div>
-  <div class="chat-form">
-    <div class="container ">
-      <form class="form-horizontal">
-        <div class="row">
-          <div class="col-sm-10 col-xs-8">
-            <input type="text" class="form-control" id="" placeholder="Mesajınızı buraya yazın..." />
+    <div class="panel-body">
+      <div class="chats">
+        <div class="chat">
+          <div class="chat-avatar">
+            <a class="avatar avatar-online" data-toggle="tooltip" href="#" data-placement="right" title="" data-original-title="June Lane">
+              <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="...">
+              <i></i>
+            </a>
           </div>
-          <div class="col-sm-2 col-xs-4">
-            <button type="submit" class="btn btn-success btn-block">Gönder</button>
+          <div class="chat-body">
+            <div class="chat-content">
+              <p>
+                Good morning, sir.
+                <br>What can I do for you?
+              </p>
+              <time class="chat-time" datetime="2015-07-01T11:37">11:37:08 am</time>
+            </div>
           </div>
+        </div>
+        <div class="chat chat-left">
+          <div class="chat-avatar">
+            <a class="avatar avatar-online" data-toggle="tooltip" href="#" data-placement="left" title="" data-original-title="Edward Fletcher">
+              <img src="https://bootdey.com/img/Content/avatar/avatar2.png" alt="...">
+              <i></i>
+            </a>
+          </div>
+          <div class="chat-body">
+            <div class="chat-content">
+              <p>Well, I am just looking around.</p>
+              <time class="chat-time" datetime="2015-07-01T11:39">11:39:57 am</time>
+            </div>
+          </div>
+        </div>
+        <div class="chat">
+          <div class="chat-avatar">
+            <a class="avatar avatar-online" data-toggle="tooltip" href="#" data-placement="right" title="" data-original-title="June Lane">
+              <img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="...">
+              <i></i>
+            </a>
+          </div>
+          <div class="chat-body">
+            <div class="chat-content">
+              <p>
+                If necessary, please ask me.
+              </p>
+              <time class="chat-time" datetime="2015-07-01T11:40">11:40:10 am</time>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="panel-footer">
+      <form>
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Say something">
+          <span class="input-group-btn">
+            <button class="btn btn-primary" type="button">Send</button>
+          </span>
         </div>
       </form>
     </div>
   </div>
+  <!-- End Panel Chat -->
 </div>
+</div>
+                  
 
                     </div>
                 </div>
